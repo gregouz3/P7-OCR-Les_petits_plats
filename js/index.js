7a9d7ded0c9647a643a96f87ca622005bec2386b
Gregoire <45977768+gregouz3@users.noreply.github.com>
2023-02-01 15:47:55 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import recipes from "./recipes.js";
 import Recipe from "./recipe.js";
-import selectTag from "./search.js";
+import displayTag from "./search.js";
 
 const displayRecipe = (recipe) => {
   //console.log(recipes);
@@ -14,71 +14,9 @@ const displayRecipes = () => {
   document.getElementById("recipes").innerHTML = "";
   recipes.forEach((recipe) => {
     displayRecipe(recipe);
-    
   });
-  displayTags();
+  displayTag();
 };
-
-
-
-const displayIngredients = () => {
-  const ingrs = document.querySelector(".ingredients");
-  ingrs.innerHTML = "";
-  const ingredients = [];
-  recipes.forEach((recipe) => {
-    recipe.ingredients.forEach((ingr) => {
-      ingredients.push(ingr.ingredient.toLowerCase());
-    });
-  });
-  const uniqueIngredients = [...new Set(ingredients)];
-  uniqueIngredients.forEach((ingr) => {
-    ingrs.innerHTML += `
-        <li class="ingrsC">${ingr}</li>
-      `;
-  });
-};
-
-const displayAppliances = () => {
-  const apps = document.querySelector(".appareil");
-  apps.innerHTML = "";
-  const appliances = [];
-  recipes.forEach((recipe) => {
-    appliances.push(recipe.appliance.toLowerCase());
-  });
-  const uniqueAppliances = [...new Set(appliances)];
-  uniqueAppliances.forEach((el) => {
-    const app = JSON.stringify(el);
-    apps.innerHTML += `
-        <li class="appsC">${app}</li>
-      `;
-  });
-};
-
-const displayUstensils = () => {
-  const usts = document.querySelector(".ustensiles");
-  usts.innerHTML = "";
-  const ustensils = [];
-  recipes.forEach((recipe) => {
-    recipe.ustensils.forEach((ustt) => {
-      ustensils.push(ustt.toLowerCase());
-    });
-  });
-  const uniqueUstensils = [...new Set(ustensils)];
-  uniqueUstensils.forEach((el) => {
-    const ust = JSON.stringify(el);
-    usts.innerHTML += `
-        <li class="ustsC">${ust}</li>
-    `;
-  });
-};
-
-const displayTags = () => {
-  displayIngredients();
-  displayAppliances();
-  displayUstensils();
-  selectTag();
-};
-
 
 displayRecipes();
 
