5de34307987ded39c8cb4cfac14278cc5091c5d3
Gregoire <45977768+gregouz3@users.noreply.github.com>
2023-02-15 12:18:21 +0100

    rm bs card

--- js/ingredient.js
+++ js/ingredient.js
@@ -4,16 +4,16 @@
     this.quantity = data.quantity;
     this.unit = data.unit;
   }
-  get displayIngredient() {
+   get displayIngredient() {
     if (this.quantity === undefined && this.unit === undefined) {
       return `
-        <li class="card-body_bot_ingredients"><span class="card-body_bot_ingredients_ingredient">${this.ingredient}</span></li>
+        <li class="recipe-body_bot_ingredients"><span class="recipe-body_bot_ingredients_ingredient">${this.ingredient}</span></li>
       `;
     } else {
       return `
-        <li class="card-body_bot_ingredients_ingredient">
-          <span class="card-body_bot_ingredients_ingredient">${this.ingredient}:</span>
-          <span class="card-body_bot_ingredients_ingredient_qt">${this.quantity || ''} ${this.unit || ''}</span>
+        <li class="recipe-body_bot_ingredients_ingredient">
+          <span class="recipe-body_bot_ingredients_ingredient">${this.ingredient}:</span>
+          <span class="recipe-body_bot_ingredients_ingredient_qt">${this.quantity || ''} ${this.unit || ''}</span>
         </li>
       `;
     }
--- js/recipe.js
+++ js/recipe.js
@@ -1,4 +1,3 @@
- 
 class Recipe {
   constructor(data) {
     this.id = data.id;
@@ -17,30 +16,27 @@ class Recipe {
       this.ingredientee += ingrediente.displayIngredient;
     });
   }
-
-  get recipeCard() {
+  get recipeCard() { 
     this.ingredient;
     return `
-    <li class="recipe col-4">
-      <div class="card">
-        <div class="card-img-top"></div>
-        <div class="card-body">
-          <div class="d-flex justify-content-between card-body_top">
-            <p class="card-body_top_name">${this.name}</p>
-            <p class="card-body_top_time">
+    <li class="recipe">
+        <div class="recipe-img-top"></div>
+        <div class="recipe-body">
+          <div class="d-flex justify-content-between recipe-body_top">
+            <p class="recipe-body_top_name">${this.name}</p>
+            <p class="recipe-body_top_time">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
                 <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
                 <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
               </svg>
-              <span class="card-body_top_time_value">${this.time} min</span>
+              <span class="recipe-body_top_time_value">${this.time} min</span>
             </p>
           </div>
-          <div class="card-body_bot d-flex justify-content-between">
-            <ul class="card-body_bot_ingredients">
+          <div class="recipe-body_bot d-flex justify-content-between">
+            <ul class="recipe-body_bot_ingredients">
             ${this.ingredientee}
             </ul>
-            <p class="card-body_bot_description">${this.description}</p>
-          </div>
+            <p class="recipe-body_bot_description">${this.description}</p>
         </div>
       </div>
     </li>
